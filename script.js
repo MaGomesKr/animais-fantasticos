b2b8fe4ee278f3747ca5d27d39b41be9964a7cfe
Matheus Gomes Toledo Pereira <matheus_toledo@globo.com>
2020-10-03 15:56:11 -0300

    Constructors(aula) - Iniciada.

--- script.js
+++ script.js
@@ -1,53 +1,105 @@
 // Navegação por tab
+let isActive = 'ativo'
+
 function startTabNav() {
-    const isActive = 'ativo'
-
-    const tabMenu = document.querySelectorAll('.js-tabmenu li')
-
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
     //Está selecionando todas as tags li que estão dentro de 'js-tabmenu'(no HTML)
     //e atribuindo a variável 'tabMenu'
 
-    const tabContent = document.querySelectorAll('.js-tabcontent section') 
-
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
     //Está selecionando todas as tags li que estão dentro de 'js-tabcontent'(no HTML)
     // e atribuindo a variável 'tabContent'
+  
+    if(tabMenu.length && tabContent.length) {
 
-    tabContent[0].classList.add(isActive)
-
+      tabContent[0].classList.add(isActive);
     //Está adicionando a classe 'ativo' no primeiro item da variável 'tabContent'(primeira 'section')
 
-    //adicionar classe de acordo com o número do item
-
     function activeTab(index) {
+        //adicionar classe de acordo com o número do item(argumento 'index')
         tabContent.forEach((section) => {
-            section.classList.remove(isActive)
+          section.classList.remove(isActive);
         });
-        tabContent[index].classList.add(isActive)
-    }
-    tabMenu.forEach((itemMenu, index) => {
+        tabContent[index].classList.add(isActive);
+      }
+  
+      tabMenu.forEach((itemMenu, index) => {
         itemMenu.addEventListener('click', () => {
-            activeTab(index)
-        })
-    })
-
-}
-
-startTabNav()
-
-function startAccordion() {
-    const accordionList = document.querySelectorAll('.js-accordion dt') //selecionar o titulo da lista
-    if(accordionList.length) {
-        accordionList[0].classList.add('ativo')
-        accordionList[0].nextElementSibling.classList.add('ativo')
-
-            function activeAccordion() {
-                this.classList.toggle('ativo')
-                this.nextElementSibling.classList.toggle('ativo')
-            }
-            accordionList.forEach((item) => {
-                item.addEventListener('click', activeAccordion)
-            })
+          activeTab(index);
+        });
+      });
     }
-}
+  }
+  startTabNav();
+    // Collapse down the hiden informations.
 
-startAccordion()
+  function startAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    let isActive = 'ativo';
+    
+    if(accordionList.length) {
+      accordionList[0].classList.add(isActive);
+      accordionList[0].nextElementSibling.classList.add(isActive);
+  
+      function activeAccordion() {
+        this.classList.toggle(isActive);
+        this.nextElementSibling.classList.toggle(isActive);
+      }
+  
+      accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion);
+      });
+    }
+  }
+  startAccordion();
+  
+  //Smooth internal link(why is it not working?) - probably chrome update
+  function startScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: 'smooth',
+      });
+  
+      // forma alternativa
+      // const topo = section.offsetTop;
+      // window.scrollTo({
+      //   top: topo,
+      //   behavior: 'smooth',
+      // });
+    }
+  
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+  startScrollSuave();
+  
+
+  // Fade in to the right when scrolling down
+  function startAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length) {
+      const windowMetade = window.innerHeight * 0.7;
+  
+      function animaScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = (sectionTop - windowMetade) < 0;
+          if(isSectionVisible)
+            section.classList.add('ativo');
+          else 
+            section.classList.remove('ativo');
+        })
+      }
+  
+      animaScroll();
+  
+      window.addEventListener('scroll', animaScroll);
+    }
+  }
+  startAnimacaoScroll();
